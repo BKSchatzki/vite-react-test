5aa2d32bce2bbcbcb325e757067cfaabc7a8602d
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-23 15:21:01 -0700

    Move Logos and Count button out of App.jsx to own components

---
+++ src/Logos.jsx
@@ -0,0 +1,29 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function Logos() {
+
+  return (
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img
+          src={viteLogo}
+          className="logo"
+          alt="Vite logo"
+        />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img
+          src={reactLogo}
+          className="logo react"
+          alt="React logo"
+        />
+      </a>
+      <p className="read-the-docs">
+        Click logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default Logos;
