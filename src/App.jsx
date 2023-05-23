38e6889fd4471851b0bb149a1949915ce2703bc1
BKSchatzki <106890912+BKSchatzki@users.noreply.github.com>
2023-05-23 16:13:28 -0700

    Add and style DayOfWeek component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import Logos from './Logos';
 import Count from './Count';
+import DayOfWeek from './DayOfWeek';
 import './App.css';
 
 function App() {
   return (
     <>
+      <DayOfWeek />
       <Logos />
       <h1>Vite + React</h1>
       <div className="card">
         <Count />
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p>Save <code>App.jsx</code> to test HMR</p>
       </div>
     </>
   )
---
+++ src/DayOfWeek.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const today = new Date();
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat(
+    'en-US',
+    { weekday: 'long' }
+  ).format(date);
+}
+
+function DayOfWeek() {
+  return (
+    <span style={{
+      background: 'linear-gradient(90deg, #00b0ff 0%, #00ffab 100%)',
+      borderRadius: '5px',
+      padding: '0.5rem 1rem'
+    }}>
+      Today is <strong>{formatDate(today)}</strong>
+    </span>
+  )
+}
+
+export default DayOfWeek;
